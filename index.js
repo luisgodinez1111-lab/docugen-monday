d6af357fb5f1194a1fdded218aba2f0f68ddc385
luisgodinez1111-lab <luisgodinez1111@gmail.com>
2026-03-12 14:22:23 -0600

    fix: allow monday.com iframe - CSP frame-ancestors + X-Frame-Options

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ app.use(express.json())
 app.use((req, res, next) => {
   res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
   res.setHeader('X-Content-Type-Options', 'nosniff');
-  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+  res.setHeader('X-Frame-Options', 'ALLOW-FROM https://monday.com');
   res.setHeader('X-XSS-Protection', '1; mode=block');
   res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
   next();
